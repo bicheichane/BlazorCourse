af0ecd4adc9ff2de46b6508c8ddd7036f543f93e
Bernardo Pinho <bernardopinho@gmail.com>
2022-11-20 17:07:54 +0000

    Juicing up the pomodoro a little bit!

--- Pages/PomodoroTimer.razor.cs
+++ Pages/PomodoroTimer.razor.cs
@@ -4,12 +4,48 @@ namespace BlazorCourse.Pages;
 
 public class PomodoroTimerBase : ComponentBase, IDisposable
 {
-    private const int PomodoroSeconds = 25 * 60;
+    private const int PomodoroSeconds = 25*60;
     private int SecondsLeft { get; set; } = PomodoroSeconds;
-    public string SecondsLeftString => TimeSpan.FromSeconds(SecondsLeft).ToString(@"mm\:ss");
     private PeriodicTimer? Timer { get; set; } = null;
-    private bool IsRunning => Timer != null;
+
+
+    protected bool IsRunning => Timer != null;
     public bool IsPaused { get; set; } = false;
+    
+    
+    public string SecondsLeftString => TimeSpan.FromSeconds(SecondsLeft).ToString(@"mm\:ss");
+    public string StartButtonText {
+        get
+        {
+            if (IsPaused)
+            {
+                return "Resume";
+            }
+            else
+            {
+                return "Start";
+            }
+        }
+    }
+
+    public string TimerTextCss
+    {
+        get
+        {
+            if (SecondsLeft <= 60)
+            {
+                return "timer-red";
+            }
+            else
+            {
+                return "timer";
+            }
+        }
+    }
+
+    public bool StartButtonDisabled => IsRunning;
+    public bool PauseButtonDisabled => !IsRunning;
+    public bool StopButtonDisabled => !IsRunning && !IsPaused;
 
     public async Task StartTimer()
     {
