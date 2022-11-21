50bfd3a6743262c0ec92f77b2883d4f2c782389b
Bernardo Pinho <bpinho@tula-health.com>
2022-11-21 20:46:03 +0000

    Trying out MudBlazor

--- Data/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorCourse.Data;
-
-public class Product
-{
-    public string Title { get; set; }
-    public decimal Price { get; set; }
-    public string Image { get; set; }
-}
---
+++ VanillaBlazor/Data/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorCourse.Data;
+
+public class Product
+{
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    public string Image { get; set; }
+}
